0fe5df2d0f92a6801445d7c6065d998fdf48468e
Sone <sthaya@gmail.com>
2020-05-20 19:43:23 -0700

    set up context to retrieve data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -5,7 +5,8 @@ class FeaturedRooms extends Component {
   static contextType = RoomContext
 
   render() {
-    
+    const { featuredRooms: rooms } = this.context
+    console.log(rooms)
 
     return (
       <div>
