977d89d7d4631ae44ee7c763c601daa01003d1db
Sone <sthaya@gmail.com>
2020-05-25 21:34:49 -0700

    set up drop down form to select room type

--- src/context.js
+++ src/context.js
@@ -9,18 +9,33 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   }
 
   // getData
 
   componentDidMount() {
     let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true)
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price))
+    let maxSize = Math.max(...rooms.map(item => item.size))
+      
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     })
   }
 
@@ -41,10 +56,29 @@ class RoomProvider extends Component {
     return room;
   }
 
+  handleChange = event => {
+    const target = event.target;
+    const value = event.type === 'checkbox' ? target.checked : target.value;
+    const name = event.target.name;
+   
+    this.setState({
+      [name]: value
+    }, this.filterRooms)
+    
+  }
+
+  filterRooms = () => {
+    console.log('hello')
+  }
+
   render() {
     return (
       <RoomContext.Provider
-        value={{ ...this.state, getRoom: this.getRoom }}>
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}>
         {this.props.children}
       </RoomContext.Provider>
     );
